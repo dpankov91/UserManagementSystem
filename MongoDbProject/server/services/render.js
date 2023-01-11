cd5155002bc491e7f614c30d157cf3c34ed54092
dpankov91 <dpankov91@gmail.com>
2023-01-11 22:35:46 +0200

    display data from mongo db

---
+++ MongoDbProject/server/services/render.js
@@ -0,0 +1,20 @@
+const axios =require('axios');
+
+
+exports.homeRoutes = (req,res) => {
+axios.get('http://localhost:3000/api/users').then(function(response){
+                            console.log(response.data[1])
+                            res.render('index', { users:response.data });       
+                        })
+                        .catch(err=>{
+                            res.send(err);
+                        })
+}
+
+exports.add_user = (req,res) => {
+    res.render('add_user');
+}
+
+exports.update_user = (req,res) => {
+    res.render('update_user');
+}
